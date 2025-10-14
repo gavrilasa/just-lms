b5b0bc097acd850045bcc66457b882e45dd332e7
gavrilasa <gavrilasamanaa@gmail.com>
2025-10-14 16:50:48 +0700

    feat(auth): Email Authentication OTP

--- app/(auth)/login/_components/LoginForm.tsx
+++ app/(auth)/login/_components/LoginForm.tsx
@@ -11,12 +11,16 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { authClient } from "@/lib/auth-client";
-import { GithubIcon, Loader } from "lucide-react";
-import { useTransition } from "react";
+import { GithubIcon, Loader, Loader2, Send } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState, useTransition } from "react";
 import { toast } from "sonner";
 
 export function LoginForm() {
+	const router = useRouter();
 	const [githubPending, startGithubTransition] = useTransition();
+	const [emailPending, startEmailTransition] = useTransition();
+	const [email, setEmail] = useState("");
 
 	async function signInWithGithub() {
 		startGithubTransition(async () => {
@@ -34,6 +38,25 @@ export function LoginForm() {
 			});
 		});
 	}
+
+	function signInWithEmail() {
+		startEmailTransition(async () => {
+			await authClient.emailOtp.sendVerificationOtp({
+				email: email,
+				type: "sign-in",
+				fetchOptions: {
+					onSuccess: () => {
+						toast.success("Email sent");
+						router.push(`/verify-request?email=${email}`);
+					},
+					onError: () => {
+						toast.error("Error sending email");
+					},
+				},
+			});
+		});
+	}
+
 	return (
 		<Card>
 			<CardHeader>
@@ -68,9 +91,27 @@ export function LoginForm() {
 				<div className="grid gap-3">
 					<div className="grid gap-2">
 						<Label htmlFor="email">Email</Label>
-						<Input type="email" placeholder="test@example.com" />
+						<Input
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+							type="email"
+							placeholder="test@example.com"
+							required
+						/>
 					</div>
-					<Button>Continue with Email</Button>
+					<Button onClick={signInWithEmail} disabled={emailPending}>
+						{emailPending ? (
+							<>
+								<Loader2 className="size-4 animate-spin" />
+								<span>Loading...</span>
+							</>
+						) : (
+							<>
+								<Send className="size-4" />
+								<span>Continue with Email</span>
+							</>
+						)}
+					</Button>
 				</div>
 			</CardContent>
 		</Card>
--- lib/auth.ts
+++ lib/auth.ts
@@ -2,6 +2,8 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./db";
 import { env } from "./env";
+import { emailOTP } from "better-auth/plugins";
+import { resend } from "./resend";
 
 export const auth = betterAuth({
 	database: prismaAdapter(prisma, {
@@ -16,4 +18,17 @@ export const auth = betterAuth({
 			clientSecret: env.AUTH_GITHUB_CLIENT_SECRET,
 		},
 	},
+
+	plugins: [
+		emailOTP({
+			async sendVerificationOTP({ email, otp }) {
+				const { data, error } = await resend.emails.send({
+					from: "gapiLMS <onboarding@resend.dev>",
+					to: [email],
+					subject: "Welcome to gapiLMS.",
+					html: `<p>Your OTP is <strong>${otp}</strong></p>`,
+				});
+			},
+		}),
+	],
 });
