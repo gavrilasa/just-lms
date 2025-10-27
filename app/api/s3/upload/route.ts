16fdf2d12abf10f3e8d70a58b814fea76bb48029
gavrilasa <gavrilasamanaa@gmail.com>
2025-10-27 16:30:58 +0700

    feat(security): Protect Endpoints

--- app/api/s3/upload/route.ts
+++ app/api/s3/upload/route.ts
@@ -5,6 +5,10 @@ import { v4 as uuidv4 } from "uuid";
 import z from "zod";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { S3 } from "@/lib/S3Client";
+import arcjet, { detectBot, fixedWindow } from "@/lib/arcjet";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { requireAdmin } from "@/app/data/admin/require-admin";
 
 export const fileUploadSchema = z.object({
 	fileName: z.string().min(1, { message: "Flename is required" }),
@@ -15,8 +19,33 @@ export const fileUploadSchema = z.object({
 	isImage: z.boolean(),
 });
 
+const aj = arcjet
+	.withRule(
+		detectBot({
+			mode: "LIVE",
+			allow: [],
+		})
+	)
+	.withRule(
+		fixedWindow({
+			mode: "LIVE",
+			window: "1m",
+			max: 5,
+		})
+	);
+
 export async function POST(request: Request) {
+	const session = await requireAdmin();
+
 	try {
+		const decision = await aj.protect(request, {
+			fingerprint: session?.user.id as string,
+		});
+
+		if (decision.isDenied()) {
+			return NextResponse.json({ error: "Dont do it dude" }, { status: 429 });
+		}
+
 		const body = await request.json();
 
 		const validation = fileUploadSchema.safeParse(body);
