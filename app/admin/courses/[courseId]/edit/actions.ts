2ebd0470512b9ef38bd104b475514d08b6860789
gavrilasa <gavrilasamanaa@gmail.com>
2025-11-02 21:04:53 +0700

    feat(courses): Create Course Structure (Drag and Drop)

--- app/admin/courses/[courseId]/edit/actions.ts
+++ app/admin/courses/[courseId]/edit/actions.ts
@@ -1,9 +1,26 @@
 "use server";
 
 import { requireAdmin } from "@/app/data/admin/require-admin";
+import arcjet, { detectBot, fixedWindow } from "@/lib/arcjet";
 import prisma from "@/lib/db";
 import { ApiResponse } from "@/lib/types";
 import { courseSchema, CourseSchemaType } from "@/lib/zodSchemas";
+import { request } from "@arcjet/next";
+
+const aj = arcjet
+	.withRule(
+		detectBot({
+			mode: "LIVE",
+			allow: [],
+		})
+	)
+	.withRule(
+		fixedWindow({
+			mode: "LIVE",
+			window: "1m",
+			max: 5,
+		})
+	);
 
 export async function editCourse(
 	data: CourseSchemaType,
@@ -12,6 +29,26 @@ export async function editCourse(
 	const user = await requireAdmin();
 
 	try {
+		const req = await request();
+
+		const decision = await aj.protect(req, {
+			fingerprint: user.user.id,
+		});
+
+		if (decision.isDenied()) {
+			if (decision.reason.isRateLimit()) {
+				return {
+					status: "error",
+					message: "You have been blocked due to rate limiting",
+				};
+			} else {
+				return {
+					status: "error",
+					message: "Looks like you are a malicious user",
+				};
+			}
+		}
+
 		const result = courseSchema.safeParse(data);
 		if (!result.success) {
 			return {
